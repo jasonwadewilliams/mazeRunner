781eef4128febc81815c6767746afe10a9b34890
Jason Williams <46290268+jasonwadewilliams@users.noreply.github.com>
2023-04-14 12:50:45 -0700

    Add files via upload

---
+++ mazeRunner_wall.py
@@ -0,0 +1,40 @@
+from guibit import Bit
+import random
+
+# Available colors:=====================================================
+# red
+# green
+# blue
+# Available functions: =================================================
+# front_clear() - checks to see if the space in front of you is clear
+# left_clear()  - checks to see if the space to the left of you is clear
+# right_clear() - checks to see if the space to the right of you is clear
+# left()  - turn to the left
+# right() - turn to the right
+# erase() - clear the current square
+# paint() - paint the current square("red")
+# get_color() - get the color of the current square
+# is_blue()  - return True if the current square is blue
+# is_green() - return True if the current square is green
+# is_red()   - return True if the current square is red
+# is_empty() - return True if the current square is empty
+
+@Bit.maze_runner(10, 10, 25)
+def maze_run(bit):
+    while not bit.is_red():
+        options = [bit.right_clear(), bit.front_clear()]
+        if options[0]:
+            path_right(bit)
+        elif options[1]:
+            bit.move()
+        else:
+            bit.left()
+
+    print("You Survived!")   
+    
+def path_right(bit):
+    bit.right()
+    bit.move()
+    
+if __name__ == '__main__':
+    maze_run(Bit.new_bit)
